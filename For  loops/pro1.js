74bcffe41fa0f9ee94bb90c9a9a07c468aa9b191
Sunny Kushwaha <123899794+kushsunny@users.noreply.github.com>
2024-04-27 04:03:30 +0530

    Add files via upload
    Task Completed For loops

---
+++ For  loops/pro1.js
@@ -0,0 +1,20 @@
+// printing hello world 10 times 
+// for(let i=1; i<=10; i++){
+//     console.log("Hello World",i,"times");
+// }
+
+
+// print one to five in horizontal manner 
+// let count="";
+// for(let i=1; i<=5; i++){
+//     count+=i+" "; 
+// }
+// console.log(count);
+
+
+//print in this manner 1 - 2 - 3 - 4 - 5 -
+let bag="";
+for(let i=1; i<=5; i++ ){
+    bag+=i+" - ";
+}
+console.log(bag);
